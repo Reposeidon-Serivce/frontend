ac1812cbac8436f4cc22af75e3c42a7b5c98d111
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-22 15:24:47 +0900

    feat: editor component's active functions
    - 텍스트 에디터 본문 초기화 기능 구현
    - 서버 데이터 페칭 시, search icon 비활성화 처리 기능 구현
    - search icon 활성화/비활성화 효과 구현
    - title input focus 되면 UI효과 디자인과 동일하게 구현
    - logo, search icon 디자인 대로 적용

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,21 +1,44 @@
 import React, { useEffect, useState } from "react";
 
+// icon
+import logoIcon from "assets/icons/logo_symbol.svg";
+import searchIcon from "assets/icons/btn_search_inactive.svg";
+import searchIconActive from "assets/icons/btn_search_active.svg";
+import eraseIcon from "assets/icons/btn_erase.svg";
+
 // style
 import "style/css/editor.css";
 
 function Editor(props: any) {
+  /* 글자 상태 관리 */
   const [titleText, setTitleText] = useState("");
   const [bodyText, setBodyText] = useState("");
   const [bodyTextLength, setBodyTextLength] = useState(0);
 
+  /* 검색 조회 상태 관리 */
+  const [activeSearch, setActiveSearch] = useState(false);
+  const [isSearchIconHover, setIsSearchIconHover] = useState(false);
+
+  // 글자수 카운팅 기능
   useEffect(() => {
     setBodyTextLength(bodyText.length);
   }, [bodyText]);
+
+  // 결과 데이터 여부에 따른 searchActive 기능 활성화/비활성화 처리
+  useEffect(() => {
+    if (props.resultData.length > 0) setActiveSearch(false);
+  }, [props.resultData]);
+
+  // 본문 에디터 초기화
+  const eraseBodyText = () => {
+    setBodyText("");
+  };
 
   return (
     <div className="editor-container ">
       <header className="editor-title-contianer">
         <div className="title-text">
+          <img className="logo-icon" src={logoIcon} alt="" />
           <span>REPOSEIDON</span>
         </div>
         <div className="sub-title-text">
@@ -25,11 +48,14 @@ function Editor(props: any) {
       <body className="editor-content-container">
         <div className="content-title-box">
           <div className="content-title-text">
-            <span>제목</span>
+            <label htmlFor="title-input">
+              <span>제목</span>
+            </label>
           </div>
           <input
             className="content-title-input"
             placeholder="제목을 입력해주세요."
+            id="title-input"
             value={titleText}
             onChange={(e: any) => setTitleText(e.target.value)}
           />
@@ -42,6 +68,12 @@ function Editor(props: any) {
           value={bodyText}
           onChange={(e: any) => setBodyText(e.target.value)}
         ></textarea>
+        <img
+          className="erase-icon"
+          src={eraseIcon}
+          alt=""
+          onClick={eraseBodyText}
+        />
         <div className="editor-footer">
           <button className="save-button">파일 저장하기</button>
           <div className="tool-box">
@@ -50,7 +82,23 @@ function Editor(props: any) {
               <span className="text-length">{bodyTextLength}자</span>
             </div>
 
-            <img src="" alt="" />
+            {!activeSearch && !isSearchIconHover ? (
+              <img
+                className="search-icon"
+                onMouseOver={() => setIsSearchIconHover(true)}
+                onMouseOut={() => setIsSearchIconHover(false)}
+                src={searchIcon}
+                alt=""
+              />
+            ) : (
+              <img
+                className="search-icon"
+                onMouseOut={() => setIsSearchIconHover(false)}
+                onClick={() => setActiveSearch(true)}
+                src={searchIconActive}
+                alt=""
+              />
+            )}
           </div>
         </div>
       </body>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,11 +8,14 @@ import Editor from "components/editor/Editor";
 import "style/css/mainPage.css";
 
 function MainPage(props: any) {
+  /* 좌우 반전 기능 상태 관리 */
   const [changeDirection, setChangeDirection] = useState(false);
 
-  useEffect(() => {
-    console.log(changeDirection);
-  }, [changeDirection]);
+  /* 조회 결과 데이터 상태 관리 */
+  const [resultData, setResultData] = useState([]);
+
+  // react-query로 데이터 페칭 기능 받아오고, resultData 업데이트 처리하는 로직 추가 개발 필요
+  // API 자체는 services에 넣고, react-query로 받아오는 기능은 hook 형태로 뺀뒤, 함수 자체만 가져와 쓰는 편이 좋을 것 같다는 생각!!
 
   return (
     <div
@@ -20,8 +23,9 @@ function MainPage(props: any) {
         !changeDirection ? "base-direction" : "reverse-direction"
       }`}
     >
+      <Editor resultData={resultData} />
       <DocumentList />
-      <Editor />
+
       <button
         className="change-direction-button"
         onClick={() => setChangeDirection(!changeDirection)}
